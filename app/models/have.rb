9415f16ff3367531b25e683e27005d3e3002077c
masarufuruya <yannwennri2@gmail.com>
2016-10-18 00:32:32 +0000

    Want,Have機能実装

---
+++ app/models/have.rb
@@ -0,0 +1,4 @@
+class Have < OwnerShip
+    belongs_to :user
+    belongs_to :item
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,18 @@ class User < ActiveRecord::Base
   has_many :followed_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followed_users, through: :followed_relationships, source: :follower
 
+  #userが作成したItem
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
+  
+  #user_idとitem_idの構成は同じなので、OwnerShipの単一継承としてHaveモデル,Wantモデルを作成
+  #ユーザーが欲しがっている商品
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items, through: :wants, source: :item
 
+  #ユーザーが持っている商品
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items, through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -29,22 +38,39 @@ class User < ActiveRecord::Base
     following_users.include?(other_user)
   end
 
-  ## TODO 実装
   def have(item)
+    if user.have_items.create(item_id: item.id)
+      flash[:sucess] = 'have'
+    else
+      flash[:danger] = 'unhave'
+    end
+    redirect_to :back
   end
 
   def unhave(item)
+    having_item = user.have_items.find_by(item_id: item.id)
+    having_item.destroy if having_item
   end
 
   def have?(item)
+    user.have_items.include?(item)
   end
 
   def want(item)
+    if user.want_items.create(item_id: item.id)
+      flash[:sucess] = 'want'
+    else
+      flash[:danger] = 'unwant'
+    end
+    redirect_to :back
   end
 
   def unwant(item)
+    wanting_item = user.want_items.find_by(item_id: item.id)
+    wanting_item.destroy if wanting_item
   end
 
   def want?(item)
+    user.want_items.include?(item)
   end
 end
