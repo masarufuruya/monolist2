f5cd80cab3e503b1c7dcb8c035f72d22fafb3b95
masarufuruya <yannwennri2@gmail.com>
2016-10-18 06:56:57 +0000

    ランキング実装

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,9 +1,9 @@
 class RankingsController < ApplicationController
   #haveの上位10個
   def have
-    have_ranking_counts = Have.group(:item_id).limit(10).order('count_item_id desc').count(:item_id) 
+    @have_ranking_counts = Have.group(:item_id).limit(10).order('count_item_id desc').count(:item_id) 
     @have_ranking_items = []
-    have_ranking_counts.each do |item_id, count|
+    @have_ranking_counts.each do |item_id, count|
       item = Item.find_by(id: item_id)
       @have_ranking_items << item unless item.nil?
     end
@@ -11,9 +11,9 @@ class RankingsController < ApplicationController
   
   #wantの上位10個
   def want
-    want_ranking_counts = Want.group(:item_id).limit(10).order('count_item_id desc').count(:item_id) 
+    @want_ranking_counts = Want.group(:item_id).limit(10).order('count_item_id desc').count(:item_id) 
     @want_ranking_items = []
-    want_ranking_counts.each do |item_id, count|
+    @want_ranking_counts.each do |item_id, count|
       item = Item.find_by(id: item_id)
       @want_ranking_items << item unless item.nil?
     end
